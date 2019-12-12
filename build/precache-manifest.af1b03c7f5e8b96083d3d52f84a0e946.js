c43c0320a80f42ca5eabaaba818c50ce4902de0c
Tarun Bansal <tarun.bansal@codalien.com>
2019-12-12 22:06:50 +0530

    build

---
+++ build/precache-manifest.af1b03c7f5e8b96083d3d52f84a0e946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4fa0b8fb31745ed23655a3c2fc308b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee50531f185479e1b517",
+    "url": "/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "d59ec17f2e8a5aac0159",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee50531f185479e1b517",
+    "url": "/static/js/2.09b17264.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.09b17264.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d59ec17f2e8a5aac0159",
+    "url": "/static/js/main.66bc24d6.chunk.js"
+  },
+  {
+    "revision": "fe17a751304812227bd4",
+    "url": "/static/js/runtime-main.64605542.js"
+  }
+]);
